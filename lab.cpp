cbf513f6eb8568732174b08f0ac6a81d51c8f7f9
LokeshV790 <lokeshvazirani29@gmail.com>
2023-04-24 11:12:40 +0530

    eval2

--- lab.cpp
+++ lab.cpp
@@ -7,62 +7,56 @@
 using namespace std;
 
 int main() {
-    // Create a file
     ofstream outfile("input.txt");
     if (!outfile) {
-        cerr << "Error: Could not create file" << endl;
+        cerr << "Could not create file" << endl;
         return 1;
     }
-    outfile << "Hello, world!" << endl;
+    outfile << "This is a parent!" << endl;
+    outfile << "This is a child!" << endl;
+    outfile << "This is a parent!" << endl;
     outfile.close();
-
-    // Generate pid and ppid of the process
     pid_t pid = getpid();
     pid_t ppid = getppid();
     cout << "PID: " << pid << endl;
     cout << "PPID: " << ppid << endl;
-
-    // Read the input file
     ifstream infile("input.txt");
     if (!infile) {
-        cerr << "Error: Could not read file" << endl;
+        cerr << "Could not read file" << endl;
         return 1;
     }
     string input;
     getline(infile, input);
     infile.close();
-
-    // Create pipes for communication
     int fd[2];
     if (pipe(fd) == -1) {
-        cerr << "Error: Could not create pipes" << endl;
+        cerr << "Could not create pipes" << endl;
         return 1;
     }
 
-    // Fork a child process
     pid_t child_pid = fork();
     if (child_pid == -1) {
-        cerr << "Error: Could not fork process" << endl;
+        cerr << "Could not fork process" << endl;
         return 1;
     }
 
-    if (child_pid == 0) { // Child process
-        close(fd[1]); // Close write end of pipe
+    if (child_pid == 0) { 
+        close(fd[1]); 
         string output;
-        getline(cin, output); // Get input from parent process
-        close(fd[0]); // Close read end of pipe
+        getline(cin, output); 
+        close(fd[0]); 
         cout << "FIFO scheduling result: " << output << endl;
         exit(0);
-    } else { // Parent process
-        close(fd[0]); // Close read end of pipe
-        write(fd[1], input.c_str(), input.length() + 1); // Write input to child process
-        close(fd[1]); // Close write end of pipe
-        wait(NULL); // Wait for child process to finish
+    } else { 
+        close(fd[0]);
+        write(fd[1], input.c_str(), input.length() + 1); 
+        close(fd[1]);
+        wait(NULL); 
         string result;
-        getline(cin, result); // Get result from child process
+        getline(cin, result); 
         ofstream outfile("output.txt");
         if (!outfile) {
-            cerr << "Error: Could not create file" << endl;
+            cerr << "Could not create file" << endl;
             return 1;
         }
         outfile << result << endl;
